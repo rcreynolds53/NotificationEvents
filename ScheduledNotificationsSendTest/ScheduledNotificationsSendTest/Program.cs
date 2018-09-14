a477bf6f9a51af61754d6ae01e18a38b82dad596
Rob Reynolds <rcreynolds53@gmail.com>
2018-09-14 06:08:29 -0500

    Initial commit

---
+++ ScheduledNotificationsSendTest/ScheduledNotificationsSendTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace ScheduledNotificationsSendTest
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://scheduled-notifications.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=cb1+aP+owbPMcvVMK20XLpmtBuGn5boWu+gv6TMZ0ho=";
+        const string ServiceBusQueueName = "Birthdays";
+        static IQueueClient queueClient;
+       
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            const int numberOfMessages = 5;
+            queueClient = new QueueClient(ServiceBusConnectionString, ServiceBusQueueName);
+
+            Console.WriteLine("======================================================");
+            Console.WriteLine("Press ENTER key to exit after sending all the messages.");
+            Console.WriteLine("======================================================");
+
+            // Send messages.
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.ScheduleMessageAsync(message,DateTimeOffset.UtcNow.AddMinutes(1));
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
